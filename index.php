518fe0a5117c8219ef80ccbf3a28317d4b369d73
Henrique Teixeira <henriqueteixeira.wd@gmail.com>
2026-01-02 09:59:05 -0300

    StartTest260102095939

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=O, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Olá Mundo!!!</h1>
+    <h1>Hellow Word!!!!</h1>
+</body>
+</html>
