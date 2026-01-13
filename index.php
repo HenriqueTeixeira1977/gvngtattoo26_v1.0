953397a0740258a99fd436f5b0ac44c453842a9f
Henrique Teixeira <henriqueteixeira.wd@gmail.com>
2026-01-13 16:34:57 -0300

    230113163552-Update

--- index.php
+++ index.php
@@ -1,10 +1,6 @@
 
 <!--  HERO COUNTER -->
 <?php include 'views/public/sections/coming-soon.php'; ?>
-<!--  CONTATO -->
-<?php include 'views/public/sections/contato.php'; ?>   
-<!--  LOCALIZAÇÃO -->
-<?php include 'views/public/sections/localizacao.php'; ?>
 
 <!--  FOOTER -->
 <?php include 'views/layouts/public_footer.php'; ?>
