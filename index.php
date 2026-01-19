1efc5368220d77696f27a905d22676466ddef047
Henrique Teixeira <henriqueteixeira.wd@gmail.com>
2026-01-19 02:08:04 -0300

    260119020627-Update Section Estilos

--- index.php
+++ index.php
@@ -2,19 +2,23 @@
 <!--  HERO COUNTER -->
 <?php include 'views/public/sections/coming-soon.php'; ?>
 
-
-
 <!--  CTA FINAL -->
 <?php include 'views/public/sections/ctafinal.php'; ?>
-<!--  PARALLAX -->
-<?php include 'views/public/sections/parallax.php'; ?>
+<!--  ESTILOS -->
+<?php include 'views/public/sections/estilos.php'; ?>
+
+
+
 
 <!--  CONTATO -->
 <?php include 'views/public/sections/contato.php'; ?>  
+
+
 <!--  LOCALIZAÇÃO -->
 <?php include 'views/public/sections/localizacao.php'; ?>
 
-
+<!--  PARALLAX -->
+<?php include 'views/public/sections/parallax.php'; ?>
 
 <!--  FOOTER -->
 <?php include 'views/layouts/public_footer.php'; ?>
